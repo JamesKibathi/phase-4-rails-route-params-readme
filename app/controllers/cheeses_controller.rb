115e6553967dfb0fdd3429dc3e5bf7e3f482e3dd
James Njenga <knjenga94@gmail.com>
2022-11-23 23:37:36 +0300

    added comments

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,10 +1,12 @@
 class CheesesController < ApplicationController
 
+  # GET /cheeses
   def index
     cheeses = Cheese.all
     render json: cheeses
   end
   
+  # Get /cheeses/:id
   def show
    # Find a cheeese using the ID from the URL
    cheese=Cheese.find(params[:id])
